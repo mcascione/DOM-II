252d04c5415c00843efd09e2b4eea59af3f67eb8
Miranda Cascione <miranda.cascione@gmail.com>
2023-03-14 16:34:37 -0400

    added image mouseover event. Event Handler #1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import './less/index.less'
 
 // Your code goes here!
+const pickDestinationImg = document.querySelector('.content-destination img');
+
+pickDestinationImg.addEventListener('mouseover', (evt)=>{  
+        evt.target.setAttribute('src', 'https://cdn.pixabay.com/photo/2015/01/15/16/17/hands-600497_1280.jpg');
+    setTimeout(() => {
+        evt.target.setAttribute('src','http://localhost:9000/img/destination.jpg');
+      },1000);
+});
+
